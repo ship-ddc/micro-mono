95ae477fbf7e9a1e962e175f4615da05e81886fa
Tom Trahan <tom@shippable.com>
2016-06-03 15:36:39 -0700

    initial upload

---
+++ nod/micro-www/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+cd /root/micro-www
+
+# # get the pre installed nodemodules to here to save demo time
+# # comment this out to do native install
+# mv /root/micro-image/node_modules .
+
+# install node modules. the above mv will make this zip through as nothing new is installed
+npm config set loglevel warn
+npm install --production
+
+# # install awscli
+# apt-get remove python-pip
+# easy_install pip
+# SUDO=$(which sudo) && $SUDO pip install awscli
