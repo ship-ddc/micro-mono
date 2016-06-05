3dd409974a965336b06789ba697e9d35d1232bfa
Tom Trahan <tom@shippable.com>
2016-06-05 12:00:07 -0700

    update message for no image change case

--- tag_image.sh
+++ tag_image.sh
@@ -16,6 +16,11 @@ detect_changed_languages() {
 detect_changed_folders() {
   folders=`git diff --name-only $SHIPPABLE_COMMIT_RANGE | sort -u | grep $1 | awk 'BEGIN {FS="/"} {print $2}' | uniq`
 
+  if [ !folders ]; then
+    echo "no image-related changes to process"
+    break
+  fi
+
   push_all_images=false
   for folder in $folders
   do
