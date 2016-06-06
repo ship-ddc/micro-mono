615011631fb16b5b4cb70ecfa7a55adbe11c1f8d
Tom Trahan <tom@shippable.com>
2016-06-05 17:53:56 -0700

    update global component

--- nod/micro-smoker/apiCall.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash
-
-#execute a Test Plan on DiveCloud at Nouvola.com
-curl -X POST -H 'Content-Type: application/json' -H 'x-api: {NOUVOLA_API_KEY}' https://divecloud.nouvola.com/api/v1/plans/{NOUVOLA_PLAN_ID}/run
